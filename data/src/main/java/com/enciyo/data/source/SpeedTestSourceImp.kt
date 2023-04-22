01a6c8332fa966c3f626fe5b73e8de63d98773a1
Mustafa <mustafa.kilic@ciceksepeti.com>
2023-04-22 16:31:31 +0300

    *

--- data/src/main/java/com/enciyo/data/source/SpeedTestSourceImp.kt
+++ data/src/main/java/com/enciyo/data/source/SpeedTestSourceImp.kt
@@ -1,104 +1,62 @@
 package com.enciyo.data.source
 
-import android.util.Log
 import com.enciyo.data.model.SpeedTestResult
-import fr.bmartel.speedtest.SpeedTestReport
+import com.enciyo.data.ext.createSpeedTestListener
+import com.enciyo.data.ext.toHumanReadable
 import fr.bmartel.speedtest.SpeedTestSocket
-import fr.bmartel.speedtest.inter.ISpeedTestListener
-import fr.bmartel.speedtest.model.SpeedTestError
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.withContext
 import java.math.BigDecimal
 import java.util.UUID
 import javax.inject.Inject
 
 class SpeedTestSourceImp @Inject constructor() : SpeedTestSource {
 
-    override fun getDownloadSpeed(host: String) = callbackFlow<SpeedTestResult> {
-        val speedTestSocket = SpeedTestSocket()
-        val transferBit =  arrayListOf<BigDecimal>()
-        val listener = object : ISpeedTestListener {
-            override fun onCompletion(report: SpeedTestReport?) {
-                Log.i("MyLogger","onCompletion")
-                val ort = transferBit.sumOf { it }.div(transferBit.size.toBigDecimal())
-                Log.i("MyLogger","onCompletion $ort")
-                trySendBlocking(SpeedTestResult.OnProgress(1f,bytesIntoHumanReadable(ort)))
+    override fun getDownloadSpeed(host: String) = getSpeed(isDownload = true, host = host)
 
-            }
+    override fun getUploadSpeed(host: String) = getSpeed(isDownload = false, host = host)
 
-            override fun onProgress(percent: Float, report: SpeedTestReport) {
-                Log.i("MyLogger","onProgress $percent ${report.transferRateBit}")
-                transferBit.add(report.transferRateBit)
-                trySendBlocking(SpeedTestResult.OnProgress(percent, bytesIntoHumanReadable(report.transferRateBit)))
-            }
+    private fun getSpeed(isDownload: Boolean, host: String) =
+        callbackFlow<SpeedTestResult> {
+            val socket = SpeedTestSocket()
+            val transferBit = arrayListOf<BigDecimal>()
+            val listener = createSpeedTestListener(
+                onCompletion = {
+                    val ort = transferBit.sumOf { it }.div(transferBit.size.toBigDecimal())
+                    trySendBlocking(SpeedTestResult.OnProgress(1f, ort.toHumanReadable()))
+                    trySendBlocking(SpeedTestResult.OnComplete)
+                },
+                onProgress = { percent, report ->
+                    transferBit.add(report.transferRateBit)
+                    trySendBlocking(
+                        SpeedTestResult.OnProgress(
+                            percent,
+                            report.transferRateBit.toHumanReadable()
+                        )
+                    )
+                },
+                onError = { _, errorMessage ->
+                    close(Throwable(errorMessage))
+                }
+            )
+            socket.addSpeedTestListener(listener)
+            if (isDownload)
+                socket.startDownload("https://$host/download?nocache${UUID.randomUUID()}&size=100000000&guid=${UUID.randomUUID()}")
+            else
+                socket.startUpload(
+                    "https://$host/upload?nocache=${UUID.randomUUID()}&guid=${UUID.randomUUID()}",
+                    1000000
+                )
 
-            override fun onError(speedTestError: SpeedTestError?, errorMessage: String?) {
-                Log.i("MyLogger","$errorMessage")
-                Log.i("MyLogger","${speedTestError?.name}")
-            }
-        }
-        speedTestSocket.addSpeedTestListener(listener)
-
-        withContext(Dispatchers.IO){
-            speedTestSocket.startDownload("https://$host/download?nocache${UUID.randomUUID()}&size=100000000&guid=${UUID.randomUUID()}")
-        }
-        awaitClose {
-            speedTestSocket.removeSpeedTestListener(listener)
-        }
-    }
-
-
-    override fun getUploadSpeed(host: String) = callbackFlow<SpeedTestResult> {
-        val speedTestSocket = SpeedTestSocket()
-        val listener = object : ISpeedTestListener {
-            override fun onCompletion(report: SpeedTestReport?) {
-                trySend(SpeedTestResult.OnComplete)
-            }
-
-            override fun onProgress(percent: Float, report: SpeedTestReport) {
-                trySend(SpeedTestResult.OnProgress(percent, bytesIntoHumanReadable(report.transferRateBit)))
-            }
-
-            override fun onError(speedTestError: SpeedTestError?, errorMessage: String?) {
-                close(Throwable(errorMessage))
+            awaitClose {
+                socket.removeSpeedTestListener(listener)
             }
         }
-        speedTestSocket.addSpeedTestListener(listener)
+            .flowOn(Dispatchers.IO)
 
-        speedTestSocket.startUpload(
-            "$host/upload?nocache=${UUID.randomUUID()}&guid=${UUID.randomUUID()}",
-            1000000
-        )
-        awaitClose {
-            speedTestSocket.removeSpeedTestListener(listener)
-        }
-    }
-        .flowOn(Dispatchers.IO)
 
-    private fun bytesIntoHumanReadable(bytes: BigDecimal): String {
-        val kilobyte: BigDecimal = (1024).toLong().toBigDecimal()
-        val megabyte = kilobyte * kilobyte
-        val gigabyte = megabyte * kilobyte
-        val terabyte = gigabyte * kilobyte
-        return if (bytes >= BigDecimal(0) && (bytes < kilobyte)) {
-            "$bytes B"
-        } else if (bytes >= kilobyte && (bytes < megabyte)) {
-            (bytes / kilobyte).toString() + " KB"
-        } else if (bytes >= megabyte && (bytes < gigabyte)) {
-            (bytes / megabyte).toString() + " MB"
-        } else if (bytes >= gigabyte && (bytes < terabyte)) {
-            (bytes / gigabyte).toString() + " GB"
-        } else if (bytes >= terabyte) {
-            (bytes / terabyte).toString() + " TB"
-        } else {
-            "$bytes Bytes"
-        }
-    }
-
-    fun Long.toBigDecimal(): BigDecimal = BigDecimal.valueOf(this)
 
 }
